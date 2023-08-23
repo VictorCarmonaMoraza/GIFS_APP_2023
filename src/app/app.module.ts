7aded5a45a856f6cd5a779eeb5a2c3e719b37ad8
Victor <VictorCarmona9035@hotmail.com>
2023-08-23 21:35:58 +0200

    8-Realizar una peticion HTTP (91)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {HttpClientModule} from '@angular/common/http';
+
 //Modulos importados
 import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
@@ -14,6 +16,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     GifsModule,
     SharedModule
   ],
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -6,15 +7,16 @@ import { Injectable } from '@angular/core';
 export class GifsService {
 
   private _tagsHisttory: string[] = [];
-  private apikey: string = 'd6aTZXKv9JX9ipehk2DnkZ2XorK7rxrN'
+  private apikey: string = 'd6aTZXKv9JX9ipehk2DnkZ2XorK7rxrN';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   get tagsHistory() {
     return [...this._tagsHisttory];
   }
 
-  private organizeHistory(tag: string):void {
+  private organizeHistory(tag: string): void {
     //Pasamos a minuscula el tag recibido
     tag = tag.toLowerCase();
 
@@ -32,14 +34,22 @@ export class GifsService {
   }
 
 
-  public searchTag(tag: string): void {
+  searchTag(tag: string): void {
 
     //Controlamos que el elemento no este vacio
     if (tag.length === 0) return;
     //LLamamos a la funcion para organizar el array de tag
     this.organizeHistory(tag);
 
-    console.log(this._tagsHisttory);
+    const parametros = new HttpParams()
+      .set('api_key', this.apikey)
+      .set('limit', '10')
+      .set('q', tag)
+
+    this.http.get(`${this.serviceUrl}/search?`, { params: parametros })
+      .subscribe(resp => {
+        console.log(resp);
+      })
 
   }
 }
