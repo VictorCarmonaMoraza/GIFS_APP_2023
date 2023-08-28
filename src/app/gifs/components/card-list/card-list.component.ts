b562e4ecd2fc224b48cd860202b025da50903ed3
Victor <VictorCarmona9035@hotmail.com>
2023-08-28 19:08:09 +0200

    *Limpieza y organizacion de codigo

--- src/app/gifs/components/card-list/card-list.component.ts
+++ src/app/gifs/components/card-list/card-list.component.ts
@@ -2,9 +2,9 @@ import { Component, Input } from '@angular/core';
 import { Gif } from '../../interfaces/gifs.interface';
 
 @Component({
-  selector: 'gifs-card-list',
-  templateUrl: './gifs-card-list.component.html',
-  styleUrls: ['./gifs-card-list.component.css']
+  selector: 'card-list',
+  templateUrl: './card-list.component.html',
+  styleUrls: ['./card-list.component.css']
 })
 export class CardListComponent {
 
