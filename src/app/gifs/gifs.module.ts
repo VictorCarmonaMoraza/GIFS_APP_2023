059ba1f7b949b8b7dccbbdbf78443d24d26af9a1
Victor <VictorCarmona9035@hotmail.com>
2023-08-29 19:55:33 +0200

    16-Lazy Image - Parte 1 (104)

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
+//Componentes del modulo
 import { HomePageComponent } from './pages/home/home-page.component';
 import { SearchBoxComponent } from './components/search-box/gifs-search-box.component';
 import { CardListComponent } from './components/card-list/card-list.component';
 import { CardComponent } from './components/card/card.component';
+
+//Modulos importados
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -14,7 +19,8 @@ import { CardComponent } from './components/card/card.component';
     CardComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports:[
     HomePageComponent
